dd451f66317ed01e85087417b6ce6942f0668369
Roen Wainscoat <13713430+roenw@users.noreply.github.com>
2019-06-14 22:04:25 -1000

    Add both /var/www/ and /var/www/html/

--- setup/uninstall.php
+++ setup/uninstall.php
@@ -1,6 +1,5 @@
 <?php
 echo "[ + ] Commencing uninstallation of PiPass.";
-echo "[ + ] We're sad to see you go... please help improve this project by leaving feedback on what issues casued you to leave.";
 $GLOBALS['phpuser'] = get_current_user();
 $localPU = $GLOBALS['phpuser'];
 $sudoersline = preg_quote("$localPU ALL=(ALL) NOPASSWD: /usr/local/bin/pihole -w *, /usr/local/bin/pihole -w -d *", '/,');
@@ -19,5 +18,19 @@ if (file_exists('/etc/pihole/pihole-FTL.conf.pipass.bak')) {
 echo "[ + ] Restoring backup of pihole-FTL.conf";
 exec("sudo mv /etc/pihole/pihole-FTL.conf.pipass.bak /etc/pihole/pihole-FTL.conf");
 }
-echo "[ + ] PiPass has been uninstalled."
+
+if (file_exists('/var/www/blockpage')) {
+echo "[ + ] Removing PiPass files...";
+exec("cd /var/www/ && sudo rm -rf index.php config.php README.md setup blockpage .git");
+}
+if (file_exists('/etc/lighttpd/lighttpd.conf.pipass.bak')) {
+echo "[ + ] Restoring backup of lighttpd.conf";
+exec("sudo mv /etc/lighttpd/lighttpd.conf.pipass.bak /etc/lighttpd/lighttpd.conf");
+}
+if (file_exists('/etc/pihole/pihole-FTL.conf.pipass.bak')) {
+echo "[ + ] Restoring backup of pihole-FTL.conf";
+exec("sudo mv /etc/pihole/pihole-FTL.conf.pipass.bak /etc/pihole/pihole-FTL.conf");
+}
+echo "[ + ] PiPass has been fully uninstalled and all files have been cleaned up and removed.";
+echo "[ / ] If you don't mind, please tell us what made you uninstall by creating a new issue on our GitHub, roenw/PiPass.";
 ?>
