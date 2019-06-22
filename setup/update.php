73f3b9a724c2bc8955ac4ad69201e7c4385e8cd4
Roen Wainscoat <roenw@icloud.com>
2019-06-21 23:45:15 -1000

    Improvements to updater
    In theory, the updater will no longer overwrite your config.php file and will use git merge (or maybe git rebase in the future) to make the changes necessary to update. Still backs up the config.php file just in case.

--- setup/update.php
+++ setup/update.php
@@ -54,21 +54,13 @@ function preInstall() {
 }
 
 function update() {
-
-    echo "[ / ] Your configuration file will be backed up, but other than that:\n";
-    echo "[ / ] BY UPDATING, YOUR CONFIGURATION FILES, EVEN IF STASHED OR COMMITTED, WILL BE OVERWRITTEN. CONTINUE? [y/n]\n";
-    $handle = fopen ("php://stdin","r");
-    $line = fgets($handle);
-    if(trim($line) != "y") {
-        echo "[ - ] Aborted by user.\n";
-        exit;
-    }
-
-    echo "[ + ] Backed up your config file. \n";
     echo "[ + ] DR check succeeded, now updating PiPass... \n";
     echo "[ + ] In document root... backing up config file.\n";
     $drf_local = $GLOBALS['document_root'];
-    exec("cd $drf_local && sudo mv config.php config.php.bak");
+    exec("cd $drf_local && sudo cp config.php config.php.bak > /dev/null 2>&1 &");
+
+    echo "[ + ] Backed up your config file. \n";
+
     function get_data($url) {
         $ch = curl_init();
         $timeout = 5;
@@ -78,23 +70,14 @@ function update() {
         $data = curl_exec($ch);
         curl_close($ch);
         return $data;
-      }
+    }
 
     $latestVersion = get_data("https://apps.roen.us/pipass/currentversion/");
-    exec("cd $drf_local && git describe --exact-match --tags $(git log -n1 --pretty='%h')", $output);
-    if("v".$latestVersion === $output[0]) {
-        echo "[ / ] Aborted by PiPass updater: You are already on the latest version of PiPass, version $output[0].\n";
-        exit;
-    } else {
-        $oldVersion = $output[0];
-        echo "Updating from $oldVersion to v$latestVersion.";
-    }
+
     echo "[ + ] Collecting files for latest version v$latestVersion.\n";
-    exec("cd $drf_local && sudo git fetch --all");
-    exec("cd $drf_local && git reset --hard v$latestVersion");
-    echo "[ + ] Selected version v$latestVersion\n";
-    echo "[ + ] Update success. You are now running PiPass v$latestVersion.\n";
-    echo "[ + ] Update complete.\n";
-    echo "[ + ] NOTE: Make sure you fill out config.php or you will get stuck in a redirect loop! You might want to merge config.php and the backup I created, config.php.bak.\n";
+    exec("cd $drf_local && sudo git fetch origin");
+    echo "[ + ] Merging local changes with latest version (merge/rebase) \n";
+    exec("cd $drf_local && git merge origin/master v$latestVersion");
+    echo "[ + ] Update successful! You are now running PiPass v$latestVersion. Please check that your installation is working,then feel free to discard the backup (.bak) files.";
 }
 ?>
