cfb91e3180dfcc48d7c0253c29f7d5d252bbf569
Roen Wainscoat <13713430+roenw@users.noreply.github.com>
2019-06-22 08:07:16 -1000

    Cleaned up setup script, colors
    Now there are colors for success, warn, and fatal. The installer also removes setup and README.md on install, since those files are unlikely to be used later.

--- setup/setup.php
+++ setup/setup.php
@@ -17,19 +17,18 @@ EOL;
 
 sleep(5);
 
-echo "[ / ] Root user check...\n";
+echo "Root user check...\n";
 
 if (0 == posix_getuid()) {
-    echo "[ + ] Root user check complete\n";
+    echo "Root user check complete\n";
     preInstall();
 } else {
-    echo "[ X ] Root user check failed. Please run the script with sudo.\n";
+    echo "\033[01;31m\n FATAL: Root user check failed. Please run the script again with sudo.\033[0m\n";
     exit;
 }
 
 function preInstall() {
-
-    echo "[ / ] DR check... Please enter your web document root. (e.g. /var/www/html)\n";
+    echo "Please enter your web document root. (e.g. /var/www/html)\n";
     $handle = fopen ("php://stdin","r");
     $line = fgets($handle);
     if(trim($line)) {
@@ -37,60 +36,47 @@ function preInstall() {
     }
 
     
-    echo "[ / ] Is this the correct document root? (" .$GLOBALS['document_root'] .") [y/n]\n";
+    echo "Confirmation: Is this the correct document root? (" .$GLOBALS['document_root'] .") [y/n]\n";
     $handle = fopen ("php://stdin","r");
     $line = fgets($handle);
     if(trim($line) != "y") {
         $GLOBALS['document_root'] = trim($line);
-        echo "[ - ] DR check failed. Exiting...\n";
+        echo "\033[01;33m\n WARN: User aborted. Exiting... \033[0m\n";
     } else {
         if(is_dir($GLOBALS['document_root'])) {
             install();
         } else {
-            echo "[ - ] DR check failed. The directory does not exist. Exiting...\n";
+            echo "\033[01;31m\n FATAL: The directory does not exist. Exiting...\033[0m\n";
             exit;
         }
     }
 }
 
 function install() {
-    echo "[ + ] DR check succeeded, now installing PiPass... \n";
-    echo "[ / ] Getting current php user...\n";
+    echo "DR check succeeded, now installing PiPass... \n";
     $GLOBALS['phpuser'] = get_current_user();
     $localPU = $GLOBALS['phpuser'];
-    echo "[ + ] Current php user is " .$GLOBALS['phpuser'] .".\n";
-    echo "[ / ] Building /etc/sudoers line to add...\n";
+    echo "Current php user is " .$GLOBALS['phpuser'] .".\n";
+    echo "Building /etc/sudoers line to add...\n";
     $sudoersline = "$localPU ALL=(ALL) NOPASSWD: /usr/local/bin/pihole -w *, /usr/local/bin/pihole -w -d *";
-    echo "[ / ] Checking if /etc/sudoers is already set up...\n";
+    echo "Checking if /etc/sudoers is already set up...\n";
     $sudoersRes = exec("sudo cat /etc/sudoers | grep /usr/local/bin/pihole");
     if(empty($sudoersRes)) {
-        echo "[ / ] Adding line to /etc/sudoers...\n";
+        echo "Adding line to /etc/sudoers...\n";
         exec("echo '$sudoersline' | sudo tee -a /etc/sudoers");
-        echo "[ + ] Permissions have been set up successfully!\n";
+        echo "Permissions have been set up successfully!\n";
     } else {
-        echo "[ / ] /etc/sudoers is already set up... not performing action.\n";
+        echo "/etc/sudoers is already set up. No need to add another line.\n";
     }
-    echo "[ / ] Now making sure that your document root folder is clear...\n";
+
     $drf_local = $GLOBALS['document_root'];
     $drfiles = exec("ls $drf_local | grep -E 'index.html|index.php'");
     if(!empty($drfiles)) {
-        echo "[ - ] It looks like there are index files in your webroot. Such as index.php, index.html, etc. Please remove them or change their name to continue installation.\n";
+        echo "\033[01;31m\n FATAL: There are index files in the webroot your specified, such as index.php or index.html. To avoid overwriting them, the PiPass installer has exited. Please remove them or rename them and return to the installation script. \033[0m\n";
         exit;
     }
 
-    $drfiles = exec("ls $drf_local | grep pipass");
-    if(!empty($drfiles)) {
-        echo "[ - ] It looks like there are already PiPass files in your webroot. (files containing 'pipass' in name trigger this warning)\n";
-        exit;
-    }
-
-    $drfiles = exec("ls $drf_local | grep setup");
-    if(!empty($drfiles)) {
-        echo "[ - ] It looks like there are already setup files in your webroot. (files containing 'setup' in the name trigger this warning)\n";
-        exit;
-    }
-
-    echo "[ + ] In document root... downloading files.\n";
+    echo "Setting up Git repository and collecting PiPass files.\n";
     exec("cd $drf_local && sudo git init .");
     exec("cd $drf_local && sudo git remote add -t \* -f origin https://github.com/roenw/pipass.git");
     exec("sudo git pull origin master");
@@ -106,30 +92,34 @@ function install() {
       }
 
     $latestVersion = get_data("https://apps.roen.us/pipass/currentversion/");
-    echo "[ + ] Files downloaded. Selecting version v$latestVersion\n";
+    echo "Files downloaded. Moving to latest stable release v$latestVersion\n";
     exec("cd $drf_local && sudo git checkout tags/v$latestVersion");
     if ( file_exists('/etc/lighttpd/lighttpd.conf') == true ) {
-		echo "[ + ] Lighttpd web server detected. Modifying 404 redirects.\n";
+		echo "Lighttpd web server detected. Modifying 404 redirects.\n";
 		if (!file_exists('/etc/lighttpd/lighttpd.conf.pipass.bak')) {
-			echo "[ + ] No lighttpd.conf backup found for PiPass. Backing up before modifying.\n";
+			echo "No lighttpd.conf backup found for PiPass. Backing up before modifying.\n";
 			exec("sudo cp /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.pipass.bak");
 		}
 		exec('sudo sed -i /etc/lighttpd/lighttpd.conf -re \'s/(server.error-handler-404[^"]*")([^"]*)(")/\1index\.php\3/\'');
 	} else {
-		echo "[ - ] Did not detect PiHole's default webserver 'Lighttpd'. Please configure installed web server to redirect 404's to PiPass.\n";
+        echo "\033[01;33m\n WARN: Didn't detect lighttpd webserver and was unable to modify 404 settings. Please manually set 404s to redirect to the webroot. \033[0m\n";
 	}
 	if (file_exists('/etc/pihole/pihole-FTL.conf')) {
-		echo "[ + ] Modifying PiHole FTL to BLOCKINGMODE=IP\n";
+		echo "Modifying PiHole FTL to BLOCKINGMODE=IP\n";
 		if (!file_exists('/etc/pihole/pihole-FTL.conf.pipass.bak')) {
-			echo "[ + ] No pihole-FTL.conf backup found for PiPass. Backing up before modifying.\n";
+			echo "No pihole-FTL.conf backup found for PiPass. Backing up before modifying.\n";
 			exec("sudo cp /etc/pihole/pihole-FTL.conf /etc/pihole/pihole-FTL.conf.pipass.bak");
 		}
 		exec('sudo sed -i \'/^BLOCKINGMODE=/{h;s/=.*/=IP/};${x;/^$/{s//BLOCKINGMODE=IP/;H};x}\' /etc/pihole/pihole-FTL.conf');
 	} else {
-		echo "[ - ] Did not detect PiHole FTL.\n";
+        echo "\033[01;33m\n WARN: Unable to detect pihole-FTL.conf. Do you have Pi-Hole installed? \033[0m\n";
 	}
-    echo "[ + ] Selected version v$latestVersion\n";
-    echo "[ + ] Installation complete. Please set your webserver to redirect all 404 pages to the homepage (web root). This function is not automated yet.\n";
-    echo "[ + ] NOTE: Make sure you fill out config.php or you will get stuck in a redirect loop!\n";
+    echo "Selected version v$latestVersion\n";
+
+    exec("cd $drf_local && sudo rm -r README.md setup ");
+    echo "Removed redundant files.";
+
+    echo "\033[01;32m\n Installation complete. \033[0m\n";
+    echo "NOTE: Make sure you fill out config.php or you will get stuck in a redirect loop!\n";
 }
 ?>
