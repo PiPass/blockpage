5a766f8a8c5de068c04bd00ec20ab504adaa0a7c
VoIP <VoIP@users.noreply.github.com>
2019-09-02 15:10:14 -0500

    Update index.php
    Apon testing.. noticed that the site wasn't being whitelisted correctly.  $url is the only thing that needed to be checked correctly.

--- blockpage/index.php
+++ blockpage/index.php
@@ -6,7 +6,7 @@ require("../locale/locale-$usrLanguage.php");
 
 if(isset($_GET['url'])) {
   $url = htmlentities($_GET['url'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
-  $GLOBALS['url'] = htmlentities($_GET['url'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
+  $GLOBALS['url'] = $_GET['url'];
   $url_provided = true;
 } else {
   $url_provided = false;
